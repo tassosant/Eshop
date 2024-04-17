109e02c130d636b684f7464c8134a6fc390d081c
tasos <tasos.antoniou@tgps.gr>
2024-04-17 12:29:12 +0300

    Completed Login Form

--- Eshop/Forms/LoginForm.cs
+++ Eshop/Forms/LoginForm.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Eshop.Models.Account;
+using Eshop.Services;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,14 +15,50 @@ namespace Eshop.Forms
 {
     public partial class LoginForm : Form
     {
+        LoginFormService service;
         public LoginForm()
         {
             InitializeComponent();
+            InitProperties();
+            
+        }
+
+        private void InitProperties()
+        {
+            this.ErrorLoginLabel.Hide();
+            this.service = new LoginFormService();
         }
 
         private void SignInButton_Click(object sender, EventArgs e)
         {
+            if (ValidateUser())
+            {
+                Debug.WriteLine("User found");
+            }
+            else
+            {
+                Debug.WriteLine("User did not found");
+            }
+        }
 
+        private Boolean ValidateUser()
+        {
+            if (this.service != null)
+            {
+                UserAbstract user = this.service.GetUser(this.UserNameTextBox.Text);
+                if (user != null )
+                {
+                    if(user.Password == this.PasswordTextBox.Text) {
+                        return true;
+                    }
+                    else
+                    {
+                        this.ErrorLoginLabel.Show();
+                        return false;
+                    }
+                }                
+            }
+            return false;
         }
 
         
--- Eshop/Services/LoginFormService.cs
+++ Eshop/Services/LoginFormService.cs
@@ -10,9 +10,22 @@ namespace Eshop.Services
 {
     internal class LoginFormService
     {
-        public LoginFormService() { }
+        UserRepository userRepository { get; set; }
+        public LoginFormService() { 
+            userRepository = new UserRepository();
+        }
 
-        
+        public UserAbstract GetUser(string username)
+        {
+            UserAbstract user = null;
+            user = this.userRepository.GetUserByUserName(username);
+                
+            if(user!= null)
+            {
+                return user;
+            }
+            return null;
+        }
 
         
 
